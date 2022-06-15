746d8a1fb2d8a14baa769189f5ab1af28606c42d
rdmshine <rdmshine@gmail.com>
2022-06-15 16:06:53 +0900

    user, blog admin set up

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import User, UserProfile
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "join_date")
+    list_display_links = ("username", "email", "join_date")
+    list_filter = ("join_date",)
+    search_fields = ("username", "email", "join_date")
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "fullname",
+        "nickname",
+        "zipcode",
+        "address",
+        "introduction",
+        "birthday",
+    )
+    list_display_links = (
+        "fullname",
+        "nickname",
+        "zipcode",
+        "address",
+        "introduction",
+        "birthday",
+    )
+    list_filter = ("address",)
+    search_fields = (
+        "fullname",
+        "nickname",
+        "zipcode",
+        "address",
+        "introduction",
+        "birthday",
+    )
